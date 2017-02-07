e84206c12ab9b5371cfe1557ac5b2b6506a7b79d
Mohan-Chandra-Bora <bora.mohan@gmail.com>
2017-02-07 15:48:15 +0530

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from 'angular2/core';
 @Component({
 selector: 'my-app',
-template: '<h1>Hello World!</h1>'
+template: '<h1>KSS session on Anular 2 to Azure</h1>'
 })
 export class AppComponent { }
